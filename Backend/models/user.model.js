350b2f448d80371dc83f14f116c0586a6ed1dd71
Ravindra Prajapat <75526037+ravindrakumardev@users.noreply.github.com>
2026-02-21 00:46:20 +0530

    done

---
+++ Backend/models/user.model.js
@@ -0,0 +1,18 @@
+import db from "../db/connection.js";
+
+
+
+export const createUser = async (name, email, password) => {
+    try {
+        const result = await db.query(  
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [name, email, password]
+        );
+        return result.rows[0];
+    } catch (err) {
+        console.error('Error creating user', err);
+        throw err;
+    }       
+};
+
+
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pkg from "pg";
+const { Pool } = pkg;   
+import pool from "./db/connection.js";
+import router from "./routes/auth.route.js";    
 import dotenv, { config, configDotenv } from "dotenv";
 
 configDotenv();
 
+
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("server is working fine !");
-})
+
+
+
+pool.connect();
+
+
+app.use ("/api/auth", router);
+
 
 app.listen(PORT ,() =>{
     console.log(`server is running on port ${PORT} ..`);

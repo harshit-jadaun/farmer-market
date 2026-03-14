54a86d96bb4d8c43863ca82eacf73b3128492799
Ravindra Prajapat <75526037+ravindrakumardev@users.noreply.github.com>
2026-03-14 23:12:27 +0530

    fix api

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -6,18 +6,10 @@ import { generateToken } from "../utils/genrate.token.js";
 configDotenv();
 //for creating a user
 export const register = async (req, res) => {
-<<<<<<< HEAD
-    const { name, email, password } = req.body;
-=======
     const { name, password, mobile_no, otp, role } = req.body;
->>>>>>> working
     try {
         //hashing password 
         const hashedPassword = await bcrypt.hash(password, 10);
-<<<<<<< HEAD
-        const newUser = await createUser(name, email, hashedPassword);
-        res.status(201).json({ message: "User registered successfully", user: newUser });
-=======
         // finally create user
         const newUser = await createUser(name, hashedPassword, mobile_no, otp, role);
 
@@ -32,14 +24,9 @@ export const register = async (req, res) => {
             newUser
 
         });
->>>>>>> working
 
     } catch (err) {
         console.error('Error registering user', err);
         res.status(500).json({ message: "Error registering user" });
-<<<<<<< HEAD
-
-=======
->>>>>>> working
     }
 };
